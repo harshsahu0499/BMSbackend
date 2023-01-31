649ae023f5fb2ef7f39da52aaa86278cbb88d69d
Harsh Sahu <harshsahu0499@gmail.com>
2023-01-31 16:43:44 +0100

    added search end point for user

--- backend/src/main/java/fr/epita/bms/controller/UserController.java
+++ backend/src/main/java/fr/epita/bms/controller/UserController.java
@@ -3,9 +3,12 @@ package fr.epita.bms.controller;
 import fr.epita.bms.domain.User;
 import fr.epita.bms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -25,4 +28,27 @@ public class UserController {
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
+
+//    @PutMapping("/users/{id}")
+//    public User updateUser(@PathVariable("id") Long id, @RequestBody User user){
+//        User updatedUser = userRepository.findById(id);
+//        user.setFirstName(updatedUser.getFirstName());
+//        user.setEmailAddress(updatedUser.getEmailAddress());
+//        // update other fields as needed
+//        return userRepository.save(user);
+//    }
+
+    @GetMapping(path="/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<User>> get(@PathVariable("id") String id){
+
+        try {
+            List<User> questionDTO = userRepository.findById(Integer.parseInt(id));
+            return ResponseEntity.ok(questionDTO);
+        }catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
+
 }
--- backend/src/main/java/fr/epita/bms/domain/User.java
+++ backend/src/main/java/fr/epita/bms/domain/User.java
@@ -9,7 +9,7 @@ public class User {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
